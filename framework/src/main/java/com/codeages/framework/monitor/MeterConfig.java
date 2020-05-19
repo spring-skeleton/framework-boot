513308f9129e5d250cb8c68e96e7d0be11756fb1
Jialong Yang <yangjialong.me@gmail.com>
2020-05-19 09:22:44 +0800

    change metric instanceId from ip to hostName

--- framework/src/main/java/com/codeages/framework/monitor/MeterConfig.java
+++ framework/src/main/java/com/codeages/framework/monitor/MeterConfig.java
@@ -8,11 +8,7 @@ import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCusto
 import org.springframework.stereotype.Component;
 
 import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
+
 
 @Component
 @Slf4j
@@ -21,32 +17,12 @@ public class MeterConfig implements MeterRegistryCustomizer {
     @Value("${server.port:8080}")
     private String port;
 
+
     @SneakyThrows
-    @Override
     public void customize(MeterRegistry registry) {
-        String[] hostAddress = getLocalIPs();
-        String instanceId = hostAddress[0] + "_" + port;
+        String hostName = InetAddress.getLocalHost().getHostName();
+        String instanceId = hostName+ "_" + port;
         log.info("设置metrics实例id:" + instanceId);
         registry.config().commonTags("instance-id", instanceId);
-    }
-
-    public String[] getLocalIPs() throws SocketException {
-        List<String> list = new ArrayList<>();
-        Enumeration<NetworkInterface> enumeration = NetworkInterface.getNetworkInterfaces();
-        while (enumeration.hasMoreElements()) {
-            NetworkInterface intf = enumeration.nextElement();
-            if (intf.isLoopback() || intf.isVirtual()) {
-                continue;
-            }
-            Enumeration<InetAddress> inets = intf.getInetAddresses();
-            while (inets.hasMoreElements()) {
-                InetAddress addr = inets.nextElement();
-                if (addr.isLoopbackAddress() || !addr.isSiteLocalAddress() || addr.isAnyLocalAddress()) {
-                    continue;
-                }
-                list.add(addr.getHostAddress());
-            }
-        }
-        return list.toArray(new String[0]);
     }
 }
